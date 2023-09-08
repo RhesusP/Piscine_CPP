b18e1e1e56c551a4477244bd784c3b156294ce8f
Camille Bernot <cbernot@student.42lyon.fr>
2023-09-08 09:07:19 +0200

    add some tests

--- module_03/ex00/main.cpp
+++ module_03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 14:04:57 by cbernot           #+#    #+#             */
-/*   Updated: 2023/07/28 15:26:22 by cbernot          ###   ########.fr       */
+/*   Updated: 2023/09/08 09:05:39 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,22 @@ int	main(void) {
 	ClapTrap	todd("Todd");
 
 	bob.attack("Todd");
-	todd.takeDamage(1);
+	todd.takeDamage(0);
 	std::cout << bob << std::endl << todd << std::endl;
+	
+	std::cout << std::endl;
+	todd.takeDamage(10);
+	std::cout << todd << std::endl;
+
+	std::cout << std::endl;
+
+	for (int i = 0; i < 10; i++) {
+		bob.attack("Todd");
+	}
+	std::cout << bob << std::endl << std::endl;
+
+	todd.beRepaired(10);
+	std::cout << todd << std::endl;
+	
 	return (0);
 }
