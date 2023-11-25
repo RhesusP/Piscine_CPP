63441a01088c8f4c97f6232eea400b05df19f0e1
cbernot <camil.bernot.cb@gmail.com>
2023-11-25 14:02:36 +0100

    ✅cmodule 07 done

--- module_07/ex02/inc/Array.hpp
+++ module_07/ex02/inc/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 11:08:23 by cbernot           #+#    #+#             */
-/*   Updated: 2023/11/09 16:48:38 by cbernot          ###   ########.fr       */
+/*   Updated: 2023/11/25 14:00:37 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
 
 class IndexOutOfBoundsException : public std::exception {
 	public:
-		virtual const char* what() const throw();
+		virtual const char* what() const throw() {
+			return ("⚠️ Index out of bounds.");
+		}
 };
 
 template<typename T>
@@ -27,14 +29,46 @@ class Array
 		unsigned int	_size;
 		T*				_array;
 	public:
-		Array(void);
-		Array(unsigned int size);
-		Array(Array const & a);
-		Array&	operator=(Array const & rhs);
-		T&	operator[](unsigned int index) const;
-		~Array(void);
+		Array(void) {
+			_size = 0;
+			_array = new T [0];
+		}
 
-		unsigned int	size(void) const;
+		Array(unsigned int size) {
+			_size = size;
+			_array = new T [size];
+		}
+
+		Array(Array const & a) {
+			this->_array = NULL;
+			*this = a;
+		}
+
+		Array&	operator=(Array const & rhs) {
+			if (_array != NULL)
+				delete [] _array;
+			this->_size = rhs.size();
+			this->_array = new T [_size];
+			for (unsigned int i = 0 ; i < rhs.size() ; i++) {
+				this->_array[i] = rhs._array[i];
+			}
+			return *this;
+		}
+		
+		T&	operator[](unsigned int index) const {
+			if (index > _size)
+				throw IndexOutOfBoundsException();
+			return this->_array[index];
+		}
+		
+		~Array(void) {
+			if (_array)
+				delete [] _array;
+		}
+
+		unsigned int	size(void) const {
+			return _size;
+		}
 };
 
 #endif
--- module_07/ex02/src/main.cpp
+++ module_07/ex02/src/main.cpp
@@ -5,14 +5,61 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/09 16:41:26 by cbernot           #+#    #+#             */
-/*   Updated: 2023/11/09 16:48:33 by cbernot          ###   ########.fr       */
+/*   Created: 2023/11/25 13:46:47 by cbernot           #+#    #+#             */
+/*   Updated: 2023/11/25 14:00:59 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/Array.hpp"
+#include <iostream>
+#include "./../inc/Array.hpp"
 
-int	main(void) {
-	std::cout << "Hello Array" << std::endl;
-	return (0);
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
 }
