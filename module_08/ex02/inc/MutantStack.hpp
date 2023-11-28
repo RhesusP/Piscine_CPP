37549c43903c1b748497fbe8f1ec0502185e2f7c
cbernot <camil.bernot.cb@gmail.com>
2023-11-28 18:43:42 +0100

    module08/ex02 in progress...

---
+++ module_08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 17:39:09 by cbernot           #+#    #+#             */
+/*   Updated: 2023/11/28 18:05:15 by cbernot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+
+template<typename T, typename C>
+class MutantStack : public std::stack {
+	private:
+
+	public:
+	
+};
+
+// TODO MutantStack::iterator<TEMPLATE>
+// begin
+// end
+// 
+
+// STD member functions 
+// (constructor)	Construct stack (public member function)
+// empty			Test whether container is empty (public member function)
+// size				Return size (public member function)
+// top				Access next element (public member function)
+// push				Insert element (public member function)
+// emplace			Construct and insert element (public member function)
+// pop				Remove top element (public member function)
+// swap				Swap contents (public member function)
+
+#endif
