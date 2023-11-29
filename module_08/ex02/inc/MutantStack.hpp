5681507bdb61aad5266d7ee5c1c7422b189a6370
cbernot <camil.bernot.cb@gmail.com>
2023-11-29 13:55:00 +0100

    08/02 in progress

--- module_08/ex02/inc/MutantStack.hpp
+++ module_08/ex02/inc/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 17:39:09 by cbernot           #+#    #+#             */
-/*   Updated: 2023/11/28 18:05:15 by cbernot          ###   ########.fr       */
+/*   Updated: 2023/11/29 13:51:14 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,33 @@
 # include <iostream>
 # include <stack>
 
-template<typename T, typename C>
-class MutantStack : public std::stack {
-	private:
-
-	public:
+template<typename T>
+class MutantStack : public std::stack<T> {
+	public :
+	typedef typename std::stack<T>::container_type::iterator	iterator;
 	
+	iterator	begin(void) const {
+		return std::stack<T>::c.begin();
+	}
+
+	iterator	end(void) const {
+		return std::stack<T>::c.end();
+	}
+
+	// private:
+
+	// public:
+	// 	MutantStack(void);
+	// 	MutantStack(MutantStack const & m);
+	// 	MutantStack&	operator=(MutantStack const & rhs);
+	// 	~MutantStack(void);
 };
 
 // TODO MutantStack::iterator<TEMPLATE>
 // begin
 // end
-// 
+// comparaison
+// incrementation / decrementation
 
 // STD member functions 
 // (constructor)	Construct stack (public member function)
---
+++ module_08/ex02/src/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 08:51:54 by cbernot           #+#    #+#             */
+/*   Updated: 2023/11/29 13:48:46 by cbernot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	// ++it;
+	// --it;
+	// while (it != ite)
+	// {
+	// 	std::cout << *it << std::endl;
+	// 	++it;
+	// }
+	// std::stack<int> s(mstack);
+	return 0;
+}
