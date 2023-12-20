bcd4a3948052cb6168e770d1b247a80f90f896ab
cbernot <camil.bernot.cb@gmail.com>
2023-12-20 09:48:33 +0100

    Fix non void return

--- module_08/ex01/src/Span.cpp
+++ module_08/ex01/src/Span.cpp
@@ -6,95 +6,116 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 14:51:09 by cbernot           #+#    #+#             */
-/*   Updated: 2023/11/28 17:53:57 by cbernot          ###   ########.fr       */
+/*   Updated: 2023/12/20 09:47:58 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Span.hpp"
 
-const char*	NotEnoughNumbersException::what(void) const throw() {
+const char *NotEnoughNumbersException::what(void) const throw()
+{
 	return ("⚠️  Not enough number to perform this operation");
 }
 
-const char* FilledListException::what(void) const throw() {
+const char *FilledListException::what(void) const throw()
+{
 	return ("⚠️  List cannot accept new numbers");
 }
 
-Span::Span(void) {
+Span::Span(void)
+{
 	_n = 0;
 	std::cout << "default constructor with size << " << _list.size() << std::endl;
 }
 
-Span::Span(unsigned int n) {
+Span::Span(unsigned int n)
+{
 	_n = n;
 }
 
-Span::Span(Span const & s) {
+Span::Span(Span const &s)
+{
 	*this = s;
 }
 
-Span&	Span::operator=(Span const & rhs) {
+Span &Span::operator=(Span const &rhs)
+{
 	_n = rhs.size();
 	_list.clear();
 	_list = std::list<int>(rhs.getList());
+	return *this;
 }
 
-Span::~Span(void) {
+Span::~Span(void)
+{
 	_n = 0;
 	_list.clear();
 }
 
-void	Span::addNumber(int number) {
-	if (_list.size() < _n) {
+void Span::addNumber(int number)
+{
+	if (_list.size() < _n)
+	{
 		_list.push_back(number);
 	}
-	else {
+	else
+	{
 		throw FilledListException();
 	}
 }
 
-void	Span::fill(std::list<int>::iterator begin, std::list<int>::iterator end, int value) {
-	while (begin != end) {
+void Span::fill(std::list<int>::iterator begin, std::list<int>::iterator end, int value)
+{
+	while (begin != end)
+	{
 		*begin = value;
 		++begin;
 	}
 }
 
-void	Span::fillRandomNumbers(void) {
-	if (_n == 0) {
+void Span::fillRandomNumbers(void)
+{
+	if (_n == 0)
+	{
 		throw NotEnoughNumbersException();
 	}
 	srand(time(NULL));
 	_list.clear();
-	for (unsigned int i = 0; i < _n ; i++) {
+	for (unsigned int i = 0; i < _n; i++)
+	{
 		int value = rand();
 		_list.push_back(value);
 	}
 }
 
-std::list<int>	Span::getList(void) const {
+std::list<int> Span::getList(void) const
+{
 	return _list;
 }
 
-unsigned int	Span::size(void) const {
+unsigned int Span::size(void) const
+{
 	return _n;
 }
 
-unsigned int	Span::shortestSpan(void) {
+unsigned int Span::shortestSpan(void)
+{
 	if (_n < 2)
 		throw NotEnoughNumbersException();
-	std::list<int>	copy = std::list<int>(_list);
+	std::list<int> copy = std::list<int>(_list);
 	std::list<int>::iterator it_begin = copy.begin();
 	std::list<int>::iterator it_end = copy.end();
-	unsigned int	distance = UINT_MAX;
-	unsigned int	new_dist;
-	int				prev = *it_begin;
-	int				curr;
+	unsigned int distance = UINT_MAX;
+	unsigned int new_dist;
+	int prev = *it_begin;
+	int curr;
 	++it_begin;
-	while (it_begin != it_end) {
+	while (it_begin != it_end)
+	{
 		curr = *it_begin;
-		new_dist = abs(curr - prev); 
-		if (new_dist < distance) {
+		new_dist = abs(curr - prev);
+		if (new_dist < distance)
+		{
 			distance = new_dist;
 		}
 		prev = *it_begin;
@@ -103,21 +124,24 @@ unsigned int	Span::shortestSpan(void) {
 	return (distance);
 }
 
-unsigned int	Span::longestSpan(void) {
+unsigned int Span::longestSpan(void)
+{
 	if (_n < 2)
 		throw NotEnoughNumbersException();
-	std::list<int>	copy = std::list<int>(_list);
+	std::list<int> copy = std::list<int>(_list);
 	std::list<int>::iterator it_begin = copy.begin();
 	std::list<int>::iterator it_end = copy.end();
-	unsigned int	distance = 0;
-	unsigned int	new_dist;
-	int				prev = *it_begin;
+	unsigned int distance = 0;
+	unsigned int new_dist;
+	int prev = *it_begin;
 	++it_begin;
-	int				curr;
-	while (it_begin != it_end) {
+	int curr;
+	while (it_begin != it_end)
+	{
 		curr = *it_begin;
-		new_dist = abs(curr - prev); 
-		if (new_dist > distance) {
+		new_dist = abs(curr - prev);
+		if (new_dist > distance)
+		{
 			distance = new_dist;
 		}
 		prev = *it_begin;
@@ -126,11 +150,13 @@ unsigned int	Span::longestSpan(void) {
 	return (distance);
 }
 
-std::ostream&	operator<<(std::ostream & o, Span const & rhs) {
-	std::list<int>	list = rhs.getList();
+std::ostream &operator<<(std::ostream &o, Span const &rhs)
+{
+	std::list<int> list = rhs.getList();
 	std::list<int>::iterator it_begin = list.begin();
 	std::list<int>::iterator it_end = list.end();
-	while (it_begin != it_end) {
+	while (it_begin != it_end)
+	{
 		o << *it_begin << std::endl;
 		++it_begin;
 	}
