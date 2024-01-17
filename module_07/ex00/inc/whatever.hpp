b9dcf04d85baa372ada757af4c1ca6b5c8d6034c
cbernot <camil.bernot.cb@gmail.com>
2024-01-17 15:42:59 +0100

    Fix stupid error about swap

--- module_07/ex00/inc/whatever.hpp
+++ module_07/ex00/inc/whatever.hpp
@@ -6,27 +6,30 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:09:36 by cbernot           #+#    #+#             */
-/*   Updated: 2023/11/29 12:34:40 by cbernot          ###   ########.fr       */
+/*   Updated: 2024/01/17 15:42:22 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
-# define WHATEVER_HPP
+#define WHATEVER_HPP
 
-template<typename T>
-void	swap(T &a, T &b) {
-	T	temp	=	a;
-	b = temp;
+template <typename T>
+void swap(T &a, T &b)
+{
+	T temp = a;
 	a = b;
+	b = temp;
 }
 
-template<typename T>
-T	min(T const & a, T const & b) {
+template <typename T>
+T min(T const &a, T const &b)
+{
 	return (a < b ? a : b);
 }
 
-template<typename T>
-T	max(T const & a, T const & b) {
+template <typename T>
+T max(T const &a, T const &b)
+{
 	return (a > b ? a : b);
 }
 
---
+++ module_07/ex00/src/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 15:40:57 by cbernot           #+#    #+#             */
+/*   Updated: 2024/01/17 15:40:59 by cbernot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "../inc/whatever.hpp"
+
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	return (0);
+}
