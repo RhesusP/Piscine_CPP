b5a1505eacee5058c65e7cbb8257abe5cccc201f
cbernot <camil.bernot.cb@gmail.com>
2024-02-02 17:06:59 +0100

    Fix void arg error

--- module_09/ex01/src/main.cpp
+++ module_09/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 17:14:03 by cbernot           #+#    #+#             */
-/*   Updated: 2024/01/17 14:17:06 by cbernot          ###   ########.fr       */
+/*   Updated: 2024/02/02 16:27:29 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	std::string input(argv[1]);
+	if (input.size() == 0)
+		return (1);
 	try
 	{
 		RPN calculator(input);
